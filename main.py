81a97e369992686308c314a4cc8b82c9e34ce9a3
Eli Burke <77055698+eureka5129@users.noreply.github.com>
2022-08-13 22:28:16 +0900

    Fix puyo collision when upside down

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 import numpy as np
 import sys
 import tkinter as tk
@@ -38,11 +39,11 @@ class Game:
         self.frames_to_fall = frames_to_fall
         self.frame_count = 1
         self.colors = types
-        self.falling_puyos = (None, None)
+        self.falling_puyos = [None, None]
         self.dirs_tf = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        self.dir_ = 2
+        self.dir_ = None
         self.is_gameover = False
-        self.highest_x = (self.width - 1)
+        self.highest_y = (self.height - 1)
 
     def is_over(self):
         return self.is_gameover
@@ -82,16 +83,12 @@ class Game:
         if (self.next_spin is not None) and (None not in self.falling_puyos):
             center_x = self.falling_puyos[0].get_x()
             center_y = self.falling_puyos[0].get_y()
-            dir_tf = (center_x - self.falling_puyos[1].get_x(),
-                      center_y - self.falling_puyos[1].get_y()
-                      )
-            self.dir_ = self.dirs_tf.index(dir_tf)
-            if self.next_spin == 'r':
+            if self.next_spin == 'l':
                 if self.dir_ >= 3:
                     self.dir_ = 0
                 else:
                     self.dir_ += 1
-            elif self.next_spin == 'l':
+            elif self.next_spin == 'r':
                 if self.dir_ <= 0:
                     self.dir_ = 3
                 else:
@@ -123,6 +120,8 @@ class Game:
             self.next_move = None
         # fall
         if self.frame_count == self.frames_to_fall:
+            if None not in self.falling_puyos:
+                self.falling_puyos.sort(key=lambda x: x.get_y(), reverse=True)
             for i, puyo in enumerate(self.falling_puyos):
                 if puyo is not None:
                     add = 0
@@ -130,24 +129,50 @@ class Game:
                         if (self.dirs_tf[self.dir_][1] != 0)\
                                 and (min(self.falling_puyos[0].get_y(), self.falling_puyos[1].get_y()) == puyo.get_y()):
                             add = 1
-                    if ((puyo.get_y() + (add + 2)) <= self.height) and (self.board[puyo.get_y()+1][puyo.get_x()] == None):
+                    if ((puyo.get_y() + (add + 2)) <= self.height) and (self.board[puyo.get_y() + 1][puyo.get_x()] == None):
                         puyo.set_y(puyo.get_y() + 1)
                     else:
-                        # FIXME: Not shure if this works...
                         if puyo.get_y() <= -1:
                             self.is_gameover = True
-                        if puyo.get_y() < self.highest_x:
-                            self.highest_x = puyo.get_y()
+                        if puyo.get_y() < self.highest_y:
+                            self.highest_y = puyo.get_y()
                         self.board[puyo.get_y()][puyo.get_x()] = puyo.get_color()
-                        tmp_list = list(self.falling_puyos)
-                        tmp_list[i] = None
-                        self.falling_puyos = tuple(tmp_list)
-                        # puyo-disapearing
-                        
-            if self.falling_puyos == (None, None):
-                self.falling_puyos = (self._Puyo(floor(self.width / 2), 1, choice(self.colors)),
-                                 self._Puyo(floor(self.width / 2), 0, choice(self.colors))
-                                 )
+                        self.falling_puyos[i] = None
+                        # # puyo-disapearing
+                        # targets_s = []
+                        # targets = []
+                        # for y, list_ in enumerate(self.board[self.highest_y:]):
+                        #     for x in range(len(list_)):
+                        #         if (x, y) not in targets:
+                        #             targets =[]
+                        #             def _find(x, y ,list):
+                        #                 list.append((x, y))
+                        #                 for direction in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                        #                     find_x = x + direction[0]
+                        #                     find_y = y + direction[1]
+                        #                     if (find_x in range(self.width)) and (find_y in range(self.height)):
+                        #                             if ((find_x, find_y) not in list) and (self.board[find_y][find_x] == self.board[y][x]):
+                        #                                 _find(find_x, find_y, list)
+                        #             _find(x, y, targets)
+                        #             targets_s.append(targets)
+                        # for list_ in targets_s:d
+                        #     if len(list_) >= 4:
+                        #         for tf in list_:
+                        #             self.board[tf[1]][tf[0]] = None
+                        # # puyo-dropping
+                        # for y, list_ in enumerate(self.board[self.highest_y::-1]):
+                        #     if np.all(self.board[self.highest_y:(y + 1)] == None):
+                        #         self.highest_y = (y + 1)
+                        #         break
+                        #     else:
+                        #         for x, color in enumerate(list_):
+                        #             if color == None:
+                        #                 self.board[y][x], self.board[y - 1][x] = self.board[y - 1][x], self.board[y][x]
+            if self.falling_puyos == [None, None]:
+                self.falling_puyos = [self._Puyo(floor(self.width / 2), 1, choice(self.colors)),
+                                      self._Puyo(floor(self.width / 2), 0, choice(self.colors))
+                                     ]   
+                self.dir_ = 2
             self.frame_count = 1
         else:
             self.frame_count += 1
